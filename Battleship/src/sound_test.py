91c30c80c62d819e20673545a40ea1ae1d0e57e5
xavR <78248195+xxruyle@users.noreply.github.com>
2024-09-29 13:30:13 -0500

    Delete Battleship/src/sound_test.py

--- Battleship/src/sound_test.py
+++
@@ -1,9 +0,0 @@
-#python3 -m venv battleship-dev
-#source battleship-live/bin/activate
-#pip3 install PyObjC
-#pip3 install playsound
-
-from playsound import playsound
-
-playsound("Battleship/src/sound_files/hit.wav")
-print("playing sound")
